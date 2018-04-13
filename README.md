c47ca146ed2de24c9e514d69edf037f5170ba68a
Brendan Boyle <brendancboyle@me.com>
2018-04-13 18:35:59 -0400

    Added react-native project

---
+++ README.md
@@ -0,0 +1,6 @@
+# react-navigation Bug #3956 Reproduction
+
+## Steps to reproduce
+
+1. `react-native init ReactNavigationBug3956`
+2. 
---
+++ index.js
@@ -0,0 +1,4 @@
+import { AppRegistry } from 'react-native';
+import App from './App';
+
+AppRegistry.registerComponent('ReactNavigationBug3956', () => App);
